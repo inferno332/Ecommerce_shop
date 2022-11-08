f544caf7d4a956d9cd64543e74c48cb1a2593272
Khoa Huynh <49389380+inferno332@users.noreply.github.com>
2022-11-08 21:07:15 +0700

    Format Date (#40)
    * update
    
    * minor error fix
    
    * Update Delete Modal
    
    * update GUI
    
    * ok
    
    * update employeeModal
    
    * finish EditEmployeeModal
    
    * update login styles
    
    * Merge branch 'kin' of https://github.com/inferno332/Ecommerce_shop into kin
    
    * update
    
    * update Moment format Date

--- admin-dashboard/src/pages/employees/index.jsx
+++ admin-dashboard/src/pages/employees/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
+import moment from 'moment';
 
 import DataTable from '../../components/DataTable';
 import Header from '../../components/Header';
@@ -23,7 +24,6 @@ const Employees = () => {
     const updateData = (data, params) => {
         try {
             axiosJWT.patch(`http://localhost:9000/employees/${params.row._id}`, data);
-            console.log(params.row._id);
             setRefesh(true);
         } catch (error) {
             console.log(error);
@@ -31,26 +31,44 @@ const Employees = () => {
     };
 
     useEffect(() => {
-        axiosJWT
-            .get('http://localhost:9000/employees')
-            .then((res) => setEmployees(res.data))
-            .catch((err) => console.log(err));
+        const fetchData = async () => {
+            try {
+                const res = await axiosJWT.get('http://localhost:9000/employees');
+                setEmployees(res.data);
+                console.log(employees);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [refresh]);
 
     const columns = [
         { field: '_id', headerName: 'Employees ID', width: 180 },
         { field: 'fullName', headerName: 'Name', flex: 0.5, cellClassName: 'name-column--cell' },
-        { field: 'email', headerName: 'Email', flex: 0.85 },
+        { field: 'email', headerName: 'Email', flex: 0.6 },
         { field: 'phoneNumber', headerName: 'Phone', flex: 0.5 },
         { field: 'address', headerName: 'Address' },
-        { field: 'birthday', headerName: 'Birthday',flex: 0.5 },
+        {
+            field: 'birthday',
+            headerName: 'Birthday',
+            renderCell: (params) => moment(params.row.birthday).format('DD-MM-YYYY'),
+        },
         {
             field: 'action',
             headerName: 'Actions',
             flex: 0.85,
             minWidth: 150,
             renderCell: (params) => {
-                return <ActionsRow content='Employee' params={params} handleDelete={handleDelete} updateData={updateData} />;
+                return (
+                    <ActionsRow
+                        content="Employee"
+                        params={params}
+                        handleDelete={handleDelete}
+                        updateData={updateData}
+                    />
+                );
             },
         },
     ];
@@ -58,13 +76,7 @@ const Employees = () => {
     return (
         <Box m="20px">
             <Header title="Employees" subtitle="List of Employees" />
-            <DataTable
-                rows={employees}
-                columns={columns}
-                getRowId={(row) => row._id}
-                styling
-                disableSelectionOnClick
-            />
+            <DataTable rows={employees} columns={columns} getRowId={(row) => row._id} styling disableSelectionOnClick />
         </Box>
     );
 };
