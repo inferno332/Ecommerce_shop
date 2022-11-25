3b2c357fd8efed34f43a9947d196dc213a516d7b
Khoa Huynh <bhuynhcongkhoa@gmail.com>
2022-11-25 08:44:18 +0700

    update layout

--- gui/pages/index.js
+++ gui/pages/index.js
@@ -3,11 +3,9 @@ import httpRequest from '../ultis/axios';
 import Head from 'next/head';
 import Benefit from '../components/Benefit';
 import Sliders from '../components/Sliders';
-import Header from '../components/Header';
 import Categories from '../components/Categories';
 import Products from '../components/Products';
 import Brands from '../components/Brands';
-import Footer from '../components/Footer';
 
 export default function Home({ sliders, categories, products, suppliers }) {
     return (
@@ -18,15 +16,11 @@ export default function Home({ sliders, categories, products, suppliers }) {
                 <link rel='icon' href='/favicon.ico' />
             </Head>
 
-            <Header />
-            <main className='container mx-auto'>
-                <Sliders sliders={sliders} />
-                <Benefit />
-                <Categories categories={categories} />
-                <Products products={products} />
-                <Brands suppliers={suppliers} />
-            </main>
-            <Footer />
+            <Sliders sliders={sliders} />
+            <Benefit />
+            <Categories categories={categories} />
+            <Products products={products} />
+            <Brands suppliers={suppliers} />
         </div>
     );
 }
