2b7dc06fbb2261a53e007b4cd18c863df6af7ecf
Kin2808 <kin0905152701@gmail.com>
2022-11-25 08:44:33 +0700

    .

--- gui/components/Footer.js
+++ gui/components/Footer.js
@@ -7,22 +7,29 @@ import { AiFillInstagram } from 'react-icons/ai';
 
 const Footer = () => {
     return (
-        <div className='container mx-auto grid grid-cols-1 gap-3 mt-10 py-5 bg-[#2B3445]'>
-            <div className='flex items-center gap-3 text-3xl text-[#f9f9f9]'>
-                <BsFacebook />
-                <AiFillInstagram />
-                <BsYoutube />
-                <SiGmail />
-            </div>
-            <div className='flex justify-between items-center'>
-                <div className='text-white [&>p]:opacity-70'>
-                    <h1 className='font-bold text-xl'>Contract Us</h1>
-                    <p className=''>Address: 38 Yen Bai, Danang, Vietnam</p>
-                    <p className=''>Email: shoes-shop@gmail.com</p>
-                    <p className=''>Phone: 0905123456</p>
+        <div className='bg-[#2B3445]'>
+            <div className='container mx-auto grid grid-cols-1 gap-5 mt-10 py-5 '>
+                <div>
+                    <Image src='/ss-logo-white.svg' alt='logo' width='100' height='100' className='md:block' />
+                    <div className='text-[#f9f9f9] opacity-70 max-w-md'>
+                        Independent since 2022, we empower people through sport and craftsmanship to create positive
+                        change in comm unities around the world.
+                    </div>
                 </div>
-                <Image src='/ss-logo.svg' alt='logo' width={60} height={40} className=' md:block h-[44px]' />
-
+                <div className=''>
+                    <div className='text-white [&>p]:opacity-70'>
+                        <h1 className='font-bold text-xl'>Contract Us</h1>
+                        <p className=''>Address: 38 Yen Bai, Danang, Vietnam</p>
+                        <p className=''>Email: shoes-shop@gmail.com</p>
+                        <p className=''>Phone: 0905123456</p>
+                    </div>
+                </div>
+                <div className='flex items-center gap-4  text-3xl text-[#f9f9f9]'>
+                    <BsFacebook className='cursor-pointer' />
+                    <AiFillInstagram className='cursor-pointer' />
+                    <BsYoutube className='cursor-pointer' />
+                    <SiGmail className='cursor-pointer' />
+                </div>
             </div>
         </div>
     );
