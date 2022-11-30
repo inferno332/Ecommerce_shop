c6d519182a1beb016f6fcd7e58fabd641adb7ceb
Khoa Huynh <bhuynhcongkhoa@gmail.com>
2022-11-30 08:16:05 +0700

    update

--- gui/components/SelectMenu.js
+++ gui/components/SelectMenu.js
@@ -8,9 +8,9 @@ export default function SelectMenu() {
     const [selected, setSelected] = useState(CategoryMenu[0]);
 
     return (
-        <div className='bg-[#F2F2F2] w-30 sm:w-40 h-fit hover:cursor-pointer z-[2] border-l border-gray-300'>
+        <div className='bg-[#F2F2F2] w-36 sm:w-40 h-fit hover:cursor-pointer z-[2] border-l border-gray-300'>
             <Listbox value={selected} onChange={setSelected}>
-                <Listbox.Button className='relative py-3 w-36'>
+                <Listbox.Button className='relative py-3 w-32'>
                     <p>{selected.name}</p>
                     <span className='absolute inset-y-0 right-0 flex items-center'>
                         <FiChevronDown className='h-5 w-5 text-gray-400' aria-hidden='true' />
@@ -20,26 +20,24 @@ export default function SelectMenu() {
                     as={Fragment}
                     leave='transition ease-in duration-100'
                     leaveFrom='opacity-100'
-                    leaveTo='opacity-0'
-                >
-                    <Listbox.Options className='fixed mt-1 max-h-60 w-60 overflow-auto rounded-md bg-white py-1 shadow-lg'>
+                    leaveTo='opacity-0'>
+                    <Listbox.Options className='fixed mt-1 max-h-60 w-60 overflow-auto rounded-md bg-white py-1 shadow-slate-700 shadow-2xl'>
                         {CategoryMenu.map((person, personIdx) => (
                             <Listbox.Option
                                 key={personIdx}
                                 className={({ active }) =>
                                     `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                        active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                                        active ? 'bg-slate-300' : 'text-gray-900'
                                     }`
                                 }
-                                value={person}
-                            >
+                                value={person}>
                                 {({ selected }) => (
                                     <>
                                         <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
                                             {person.name}
                                         </span>
                                         {selected ? (
-                                            <span className='absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600'>
+                                            <span className='absolute inset-y-0 left-0 flex items-center pl-3 text-black'>
                                                 <FiCheck className='h-5 w-5' aria-hidden='true' />
                                             </span>
                                         ) : null}
