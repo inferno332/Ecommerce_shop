fcace58571f81e86e783861f65942699f38bbad7
Khoa Huynh <49389380+inferno332@users.noreply.github.com>
2022-12-05 13:38:18 +0700

    Khoa (#95)
    * update dropdown menu
    
    * update dropdown
    
    * update search Product
    
    * update logo file
    
    * .
    
    * update layout
    
    * .
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Merge branch 'khoa' of https://github.com/inferno332/Ecommerce_shop into khoa
    
    * update filter
    
    * update filter
    
    * update search ui

--- gui/components/Header.js
+++ gui/components/Header.js
@@ -1,49 +1,45 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { FiSearch } from 'react-icons/fi';
-import { FiUser, FiShoppingCart, FiMenu } from 'react-icons/fi';
+import { FiUser, FiShoppingCart } from 'react-icons/fi';
 import DropDownMenu from './DropDownMenu';
 import MobileMenuModal from './MobileMenuModal';
+import SearchAutoComplete from './SearchAutoComplete';
 
-import SelectMenu from './SelectMenu';
 
 const Header = () => {
     return (
         <header>
             <div className='container m-auto'>
                 <div className='flex flex-col justify-center items-center m-auto'>
-                    <div className='flex justify-between items-center h-[80px] w-full gap-2 md:gap-6'>
+                    <div className='flex justify-between items-center h-16 md:h-20 w-full gap-2 md:gap-6'>
                         <Link href='/'>
                             <Image
                                 src='/ss-logo.svg'
                                 alt='logo'
                                 width='100'
                                 height='100'
-                                className='hidden md:block h-fit'
+                                className='hidden md:inline-block h-fit hover:opacity-70'
                             />
                         </Link>
-
-                        <div className='border-gray-3       00 border hover:border-orange-700 focus-within:border-orange-700 rounded-full flex items-center h-[44px] max-w-[670px] w-full hover:cursor-text truncate'>
-                            <FiSearch className='text-[#BDBDBD] text-[20px] mx-[16px]' />
-                            <input className='flex-1 min-w-0  h-full' placeholder='Search...' />
-
-                            {/* Select Menu */}
-                            <SelectMenu />
-
-                            <div className='bg-[#F2F2F2] w-fit h-fit hover:cursor-pointer'></div>
+                        {/* Search bar */}
+                        <div className='items-center flex flex-grow rounded-full bg-transparent relative'>
+                            <div className='relative w-full flex flex-row-reverse'>
+                                <SearchAutoComplete />
+                            </div>
                         </div>
 
-                        <div className='flex gap-6'>
-                            <div className='rounded-full p-4 bg-[#F2F2F2] hover:bg-[#E0E0E0] hover:cursor-pointer hidden md:block hover:animate-pulse border-gray-300 border'>
+                        <div className='flex md:gap-2 items-center'>
+                            <span className='rounded-full text-lg p-4 hover:bg-[#E0E0E0] hover:cursor-pointer hidden md:block'>
                                 <FiUser />
-                            </div>
-                            <div className='rounded-full p-4 bg-[#F2F2F2] hover:bg-[#E0E0E0] hover:cursor-pointer hidden md:block hover:animate-pulse border-gray-300 border'>
+                            </span>
+                            <span className='rounded-full text-lg p-4 hover:bg-[#E0E0E0] hover:cursor-pointer'>
                                 <FiShoppingCart />
-                            </div>
-                            <div className='rounded-full p-4 bg-[#F2F2F2] hover:bg-[#E0E0E0] hover:cursor-pointer block md:hidden border-gray-300 border'>
+                            </span>
+                            <span className='rounded-full text-lg p-4 hover:bg-[#E0E0E0] hover:cursor-pointer block md:hidden'>
                                 {/* <FiMenu /> */}
-                                <MobileMenuModal/>
-                            </div>
+                                <MobileMenuModal />
+                            </span>
                         </div>
                     </div>
 
