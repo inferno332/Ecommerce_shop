ba60a7cf4c0155eaddaa841e139a1c9dfed01bdf
Khoa Huynh <bhuynhcongkhoa@gmail.com>
2022-12-05 13:33:29 +0700

    update search ui

--- gui/components/MobileMenuModal.js
+++ gui/components/MobileMenuModal.js
@@ -2,10 +2,29 @@ import { Dialog, Transition } from '@headlessui/react';
 import { Fragment, useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
 import { IoClose } from 'react-icons/io5';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function MobileMenuModal() {
     let [isOpen, setIsOpen] = useState(false);
-
+    const Categories = [
+        {
+            name: 'All categories',
+            link: '/product',
+        },
+        {
+            name: 'Women',
+            link: '/product/filter?category=Women',
+        },
+        {
+            name: 'Men',
+            link: '/product/filter?category=Men',
+        },
+        {
+            name: 'Accessory',
+            link: '/product/filter?category=Accessory',
+        },
+    ];
     function closeModal() {
         setIsOpen(false);
     }
@@ -18,7 +37,7 @@ export default function MobileMenuModal() {
         <>
             <div className='flex items-center justify-center'>
                 <button type='button' onClick={openModal}>
-                    <FiMenu/>
+                    <FiMenu />
                 </button>
             </div>
 
@@ -45,21 +64,44 @@ export default function MobileMenuModal() {
                                 leave='transform transition ease-in-out duration-300'
                                 leaveFrom='translate-x-0'
                                 leaveTo='translate-x-full'>
+                                {/* Content */}
                                 <Dialog.Panel className='w-full relative flex flex-col justify-between max-w-md transform overflow-hidden bg-white p-6 text-left min-h-screen shadow-xl transition-all'>
-                                    <div className='absolute top-1 right-1 p-4'>
-                                        <IoClose className='text-2xl' />
+                                    <div className='absolute top-1 right-1 p-4 rounded-full truncate focus:outline-none'>
+                                        <button onClick={closeModal}>
+                                            <IoClose className='text-2xl' />
+                                        </button>
                                     </div>
-                                    <div className='gap-5 flex flex-col mt-4'>
-                                        <p className='text-xl font-semibold'>All categories</p>
-                                        <p className='text-xl font-semibold'>Women</p>
-                                        <p className='text-xl font-semibold'>Men</p>
-                                        <p className='text-xl font-semibold'>Accessories</p>
+
+                                    <div className='gap-5 flex flex-col mt-10'>
+                                        {Categories.map((category) => (
+                                            <Link href={category.link} key={category.name} legacyBehavior>
+                                                <a
+                                                    className='text-gray-900 hover:text-gray-900 text-2xl'
+                                                    onClick={closeModal}>
+                                                    {category.name}
+                                                </a>
+                                            </Link>
+                                        ))}
+                                        <Image
+                                            src='/ss-logo.svg'
+                                            alt='logo'
+                                            width='100'
+                                            height='100'
+                                            className='h-fit'
+                                        />
                                     </div>
-                                    <div>
-                                        <p>
-                                            Become a Nike Member for the best products, inspiration and stories in
-                                            sport.
-                                        </p>
+
+                                    <div className='flex gap-2'>
+                                        <Link href='/login' legacyBehavior>
+                                            <button className='text-lg px-6 py-[6px] text-white rounded-full border-[1.5px] bg-[#111]'>
+                                                Sign in
+                                            </button>
+                                        </Link>
+                                        <Link href='/register' legacyBehavior>
+                                            <button className='text-lg px-4 py-[6px] text-black rounded-full border-[1.5px] border-[#ccc] bg-white'>
+                                                Resgister
+                                            </button>
+                                        </Link>
                                     </div>
                                 </Dialog.Panel>
                             </Transition.Child>
