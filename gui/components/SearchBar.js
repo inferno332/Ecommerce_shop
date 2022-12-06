301c5f7c4086efc47ad5dd259b1f3fb4e76bdb2a
Khoa Huynh <bhuynhcongkhoa@gmail.com>
2022-12-06 10:52:56 +0700

    update search bar

--- gui/components/SearchBar.js
+++ gui/components/SearchBar.js
@@ -1,17 +1,17 @@
 import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 import { useState } from 'react';
-import {FiSearch} from 'react-icons/fi'
+import { FiSearch } from 'react-icons/fi';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import axios from 'axios';
 
 const SearchBar = () => {
     // note: the id field is mandatory
-    const [isShow, setIsShow] = useState(false);
+    const router = useRouter();
     const [searchResult, setSearchResult] = useState([]);
     const handleOnSearch = async (string, results) => {
         // onSearch will have as the first callback parameter
         // the string searched and for the second the results.
-        console.log(string, results);
         try {
             const res = await axios.get(`${process.env.BASE_URL}/products/search/${string}`);
             let tmpData = res.data.map(({ _id, ...rest }) => ({ id: _id, ...rest }));
@@ -21,20 +21,27 @@ const SearchBar = () => {
         }
     };
 
-    const handleOnSelect = (item) => {};
+    const handleOnSelect = (item) => {
+        router.push(`/product/details/${item.id}`);
+    };
 
     const formatResult = (item) => {
         return (
-            <div className='flex items-start md:items-center mr-5 flex-col md:flex-row'>
-                <Image src={`${process.env.BASE_URL}${item.imageURL[0]}`} width='200' height='200' alt={item.name} className='h-16 w-16 md:h-24 md:w-24' />
+            <div className='flex items-start md:items-center mr-5 flex-col md:flex-row cursor-pointer'>
+                <Image
+                    src={`${process.env.BASE_URL}${item.imageURL[0]}`}
+                    width='200'
+                    height='200'
+                    alt={item.name}
+                    className='h-16 w-16 md:h-24 md:w-24'
+                />
                 <p className='text-md text-gray-800'>{item.name}</p>
             </div>
         );
     };
 
     return (
-        <div className='w-full relative md:w-[180px] focus-within:w-full duration-500 z-10'
-        >
+        <div className='w-full relative md:w-[180px] focus-within:w-full duration-500 z-10'>
             <ReactSearchAutocomplete
                 items={searchResult}
                 onSearch={handleOnSearch}
@@ -51,7 +58,7 @@ const SearchBar = () => {
                 }}
             />
             <span className='absolute top-[50%] -translate-y-[50%] right-3 text-lg'>
-              <FiSearch />
+                <FiSearch />
             </span>
         </div>
     );
