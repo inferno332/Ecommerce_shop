53bf84dc989e75d3da2bf3d5a9e5bd4bf8507bea
Dat <ddatj1997@gmail.com>
2022-12-11 14:37:03 +0700

    models

--- server-side/models/product.js
+++ server-side/models/product.js
@@ -5,6 +5,7 @@ const productSchema = new Schema(
     {
         name: { type: String, required: true },
         price: { type: Number, required: true },
+        discount: { type: Number, required: true },
         categoryId: { type: Schema.Types.ObjectId, ref: 'Category' },
         supplierId: { type: Schema.Types.ObjectId, ref: 'Supplier' },
         description: { type: String, required: true },
